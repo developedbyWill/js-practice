b5f283f1c3d2ed58aa69192fe7eb0977e10741d6
developedbyWill <127629003+developedbyWill@users.noreply.github.com>
2023-05-30 01:10:55 +0000

    Linked conditionals.js to conditionals.html file and created simple arithmetic

--- conditionals.js
+++ conditionals.js
@@ -1,18 +1,5 @@
-// Array containing names
-let colleagues = ["Bernard Gyamfi", "Ernest Appiah", "Owuraku Sintim"];
-
-for (let i = 0; i < colleagues.length; i++) {
-  console.log(colleagues[i] + "br");
+function addThreeNumbers(num1, num2, num3) {
+  return num1 + num2 + num3;
 }
 
-let score = 85;
-
-if (score >= 90) {
-  console.log("A");
-} else if (score >= 80 && score < 90) {
-  console.log("B");
-} else if (score >= 70 && score < 80) {
-  console.log("C");
-} else {
-  console.log("D");
-}
+console.log(addThreeNumbers(7, 11, 13));
